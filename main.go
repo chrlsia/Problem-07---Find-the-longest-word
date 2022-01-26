c0f58d2c67a258ab9b1d48f23d36974e290cffc2
Chris Siannas <xrsia@otenet.gr>
2022-01-26 18:16:53 +0200

    Refactor with 2 functions

--- main.go
+++ main.go
@@ -7,23 +7,34 @@ import (
 	"strings"
 )
 func main() {
-  fmt.Println("Give me a sentence ->")
-  r := bufio.NewReader(os.Stdin)
-  line, err := r.ReadString('\n')
-  if err != nil {
-	panic(err)
- }
+  
 
- words := strings.Split(strings.TrimSpace(line), " ")
+ words := splitSentIntoWords()
 
- max:=words[0] //string
+ max:=findMaxWord(words)
+
+ fmt.Println(max)
+
+}
+
+func  splitSentIntoWords()[]string{
+	fmt.Println("Give me a sentence ->")
+	r := bufio.NewReader(os.Stdin)
+	line, err := r.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	words := strings.Split(strings.TrimSpace(line), " ")
+	return words
+}
+
+func findMaxWord(words []string)string{
+	max:=words[0] //string
 
  for i:=1;i<len(words);i++{
    if len(words[i])>=len(max){
      max = words[i]
    }
   }
-
- fmt.Println(max)
-
+  return max
 }
